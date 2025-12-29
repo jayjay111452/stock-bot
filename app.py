4ed71306d775dca13374466cb93817cf0531828e
jayjay111452 <153094329+jayjay111452@users.noreply.github.com>
2025-12-29 23:01:47 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -550,14 +550,7 @@ def run_analysis():
     
     with col_traffic:
         # 显示巨大的红绿灯状态
-        # --- DEBUG START ---
-        st.write("Debug: radar_result status:", radar_result) 
-
-        if radar_result is None:
-         st.error("数据获取失败：radar_result 为空！")
-         st.stop() # 停止后续渲染，防止报错
-        # --- DEBUG END ---
-        st.markdown(f"<h3 style='text-align: center; color: {radar_result['color']}'>{radar_result['status']}</h3>", unsafe_allow_html=True)
+        st.markdown(f"<h1 style='text-align: center; color: {radar_result['color']}'>{radar_result['status']}</h1>", unsafe_allow_html=True)
         st.metric("综合得分 (0-100)", f"{radar_result['score']} 分")
         st.metric("VIX 恐慌指数", f"{radar_result['vix']:.2f}")
         st.metric("CNN 贪婪指数", fng_score)
